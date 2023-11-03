af04bcf840db7e78a7f7aa4a0c078490406f8f5a
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-03 12:02:34 +0000

    add route for add symptom

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from application import app
+from application.forms import RecordForm
 
 @app.route('/')
 def index():
 	return render_template('index.html', title='Home')
+
+@app.route('/add_symptom', methods=['GET', 'POST'])
+def add_symptom():
+	form = RecordForm()
+	return render_template('add_symptom.html', title='Add Symptom', form=form)
