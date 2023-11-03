2c0f69e14a0c522af68d70b51e3aace6b00264f0
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-03 11:27:42 +0000

    home page added

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template
 from application import app
 
 @app.route('/')
---
+++ application/static/scripts/disclaimer_script.js
@@ -0,0 +1,4 @@
+function disclaimer() {
+    confirm("DISCLAIMER: This website was designed by a student programmer and while useful, does not medically evaluate pain and will NEVER offer medical advice.")
+    document.getElementById("disclaimer")
+}
